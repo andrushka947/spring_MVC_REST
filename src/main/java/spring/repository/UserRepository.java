b1a5b2e0efe9b8c983f1c00435fc4cef57b67439
Andrey <likeintel9@gmail.com>
2017-09-21 17:15:33 +0300

    smth changed

--- src/main/java/spring/repository/UserRepository.java
+++ src/main/java/spring/repository/UserRepository.java
@@ -3,12 +3,14 @@ package spring.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import spring.entity.User;
 
 import java.util.List;
 
 @Repository
+@RepositoryRestResource(path = "user")
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u from User u where u.name = :name")
